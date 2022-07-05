463e31486bd13e5cad08e9750d8a09e0de2861f0
fedechaves <federicochavesf@gmail.com>
2022-07-05 16:42:24 -0400

    new changes to project

--- server.js
+++ server.js
@@ -1,2 +1,26 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
 const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const PORT = 8000
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'star-wars-quotes',
+    collection 
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('connected to database')
+        db= client.db(dbName)
+        collection = db.collection('quotes')
+    })
+
+//DECLARE VARIABLES
+
+//CREATE PORT
+app.listen(process.env.PORT || PORT, () => { 
+    console.log(`server running on ${PORT} juju`)
+})
+
