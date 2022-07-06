218c30e8f81157df2192754ab63f802ece8bf8d3
fedechaves <federicochavesf@gmail.com>
2022-07-06 19:17:08 -0400

    add it asyn ejs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 const cors = require('cors')
+const { response } = require('express')
 require('dotenv').config()
 const PORT = 8000
 
@@ -17,12 +18,22 @@ MongoClient.connect(dbConnectionString)
         collection = db.collection('quotes')
     })
 
-//DECLARE VARIABLES
+//CALL MIDDLEWARES
 app.set('view engine', 'ejs') //templating,  generate html
 app.use(express.static('public')) //to access public folder
 app.use(express.urlencoded({extended: true })) // helping parse urls
 app.use(express.json())// help express parse json files
 app.use(cors())
+
+app.get('/', async (req,res) => {
+    try {
+        res.render('index.ejs')
+} catch(error){
+    response.status(500).send({message: error.message})
+}
+})
+
+
 
 //CREATE PORT
 app.listen(process.env.PORT || PORT, () => { 
